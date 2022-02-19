1e7dfd4ef78320c776b9cb08cb05c72700e208e8
Owen W <85974813+BlueCat00@users.noreply.github.com>
2022-02-19 16:15:59 -0500

    Removed unnecessary files

--- filterscripts/watermark.lua
+++
@@ -1,4 +0,0 @@
-function chat_connectEvent(playerid)
-    drawText(playerid, 6, 0.001, 0.96, 0.15, 0.54, "BlueCat and Bruce's RPG", 5000, 0xFFFFFFFF)
-end
-registerEvent("chat_connectEvent", "onPlayerCredential")
--- oldServer/filterscripts/watermark.lua
+++
@@ -1,5 +0,0 @@
-function chat_connectEvent(playerid)
-	drawText(playerid, 5, 0.001, 0.925, 0.15, 0.54, "~r~P~w~e~b~t~r~a~b~C~r~l~w~o~b~u~r~d ~r~R~b~P~w~G", 5000, 0xFFFFFFFF)
-    drawText(playerid, 6, 0.001, 0.96, 0.15, 0.54, "~w~Developed by ~r~Bruce ~w~and ~b~BlueCat", 5000, 0xFFFFFFFF)
-end
-registerEvent("chat_connectEvent", "onPlayerCredential")
