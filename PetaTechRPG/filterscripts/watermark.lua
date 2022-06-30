b7fc0e9f70f50923a57bac0902ddfe777b62e0c8
BlueCatIV <85974813+BlueCatIV@users.noreply.github.com>
2022-06-30 17:22:26 -0400

    Update watermark.lua

--- PetaTechRPG/filterscripts/watermark.lua
+++ PetaTechRPG/filterscripts/watermark.lua
@@ -1,5 +1,5 @@
 function chat_connectEvent(playerid)
-	drawText(playerid, 5, 0.001, 0.925, 0.15, 0.54, "~r~Peta~b~Cloud ~r~R~b~P~w~G", 5000, 0xFFFFFFFF)
+	drawText(playerid, 5, 0.001, 0.925, 0.15, 0.54, "~w~IV:MP RPG", 5000, 0xFFFFFFFF)
 	drawText(playerid, 6, 0.001, 0.96, 0.15, 0.54, "~w~Developed by ~r~Bruce ~w~and ~b~BlueCat", 5000, 0xFFFFFFFF)
 end
 registerEvent("chat_connectEvent", "onPlayerCredential")
