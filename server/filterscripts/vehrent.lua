7a1f525732f515ee9effa8e57f580ed123cd4a10
Owen W <wetzelowen39@gmail.com>
2022-07-01 14:43:09 -0400

    updates

--- server/filterscripts/vehrent.lua
+++ server/filterscripts/vehrent.lua
@@ -1,1 +1,3 @@
 createBlip(31.188123703003, -41.969669342041, 13.817593574524, 27, 0xFFFFFFFF, 1, 0, true, "Vehicle Rental")
+
+createVehicle(149, 31.188123703003, -41.969669342041, 13.817593574524, 0.0, 0.0, 0.0, rColor, rColor, rColor, rColor, 1)
